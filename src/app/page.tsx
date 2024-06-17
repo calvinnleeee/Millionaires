7dadba867af9d8d0bf7a68987bd67c8846a7a0ae
Calvin Lee <calvinnleeee@gmail.com>
2024-06-17 14:34:54 -0700

    Added logo to main page.
    Added logo with fading background to main page. Colours and other designs to be considered and played around with later.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
-import NavigationBar from "../components/main/bot"
-import TopBar from "../components/main/top"
+import NavigationBar from "../components/main/bot";
+import TopBar from "../components/main/top";
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -10,15 +11,25 @@ export default function Home() {
         </div>
 
         <div className="stuff h-full row-span-5 flex flex-col items-center justify-center">
-          <div className="logo mb-5"></div>
+          <div className="logo mb-5">
+            <Image 
+              className="mx-auto"
+              src="/img/brain.png"
+              alt="Logo"
+              width={1170}
+              height={896}
+            />
+          </div>
 
           <div className="welcome mt-5">
             <p className="text-3xl text-center">Welcome back, #user#!</p>
           </div>
 
           <div className="h-24"></div>  {/* spacer */}
-
-          {/* <div></div> */}
+          
+          <div className="splash-text">
+            <p className="text-xl text-center">Click the <span className="underline">Record</span> tab to get started with your day.</p>
+          </div>
         </div>
 
         <div className="row-span-1">
